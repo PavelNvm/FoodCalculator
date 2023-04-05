23930956801f76d629ece843025794ccce3dcad1
PavelNvm <pavelnevmerzhitskiy@gmail.com>
2023-04-05 11:59:11 +0300

    Working prototype of settings window

--- FoodCalculator/SettingsViewModel.cs
+++ FoodCalculator/SettingsViewModel.cs
@@ -18,6 +18,8 @@ namespace FoodCalculator
     {
         public ObservableCollection<int> CBE { get; set; } =new ObservableCollection<int>() { 1,2,3,4,5,6};
         public ObservableCollection<string> FoodTypes { get; set; }
+        public int BreakFastFoodQuantity { get { return BreakfastFoodTypeList.Count(); } set { if (value > 0 && value <= 6) { ChangeFoodquant(value,BreakfastFoodTypeList); OnPropertyChanged("FoodQuantity"); } } }
+        public ObservableCollection<Food> BreakfastFoodTypeList { get; set; }=new ObservableCollection<Food>() { new Food() { Id = 0 }, new Food() { Id = 1 }, new Food() { Id = 2 } };
         public MealFilling BreakfastSetting { get; set; } = new MealFilling() { FoodQuantity=3};
         public MealFilling LunchSetting { get; set; } = new MealFilling();
         public MealFilling DinnerSetting { get; set; } = new MealFilling();
@@ -40,7 +42,22 @@ namespace FoodCalculator
             });
 
         }
-        
+        public void ChangeFoodquant(int quant,ObservableCollection<Food> FL)
+        {
+            while (FL.Count != quant)
+            {
+                if (FL.Count > quant)
+                {
+                    FL.RemoveAt(FL.Count - 1);
+                }
+                else
+                {
+                    FL.Add(new Food());
+                    FL.Last().Id = FL[FL.Count - 2].Id+1;
+                }
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged = null!;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
