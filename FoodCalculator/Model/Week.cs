60e3eed6b98c5de7d3b4c81b82cc8e2977d5af5f
Nevmerzhitskiy Pavel <pavelnevmerzhitskiy@gmail.com>
2023-11-10 07:57:22 +0300

    Prep for DB

--- FoodCalculator/Model/Week.cs
+++ FoodCalculator/Model/Week.cs
@@ -1,31 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FoodCalculator.Model
 {
-    public class Week
+    public class Week : INotifyPropertyChanged
     {
         public int Id { get; set; }
-        public DateOnly FirstDay { get; set; }
-        public DateOnly LastDay { get; set; }
+        public DateOnly FirstDay { get { return firstDay; } set { firstDay = value; OnPropertyChanged(nameof(WeekSpan)); } }
+        private DateOnly firstDay;
+        public DateOnly LastDay { get { return lastDay; } set { lastDay = value; OnPropertyChanged(nameof(WeekSpan)); } }
+        private DateOnly lastDay;
+
         public Day[] DaysOfTheWeek { get; set; } = new Day[7] {new Day(), new Day(), new Day(), new Day(), new Day(), new Day(), new Day() };
-        public Week() 
+        public Week((DateOnly, DateOnly) a) 
         {
-            for(int i =0;i<7;i++)
+            FirstDay = a.Item1;
+            LastDay = a.Item2;
+            for (int i =0;i<7;i++)
             {
                 DaysOfTheWeek[i].Week_Id = Id;
             }
         }
-
+        public Week()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                DaysOfTheWeek[i].Week_Id = Id;
+            }
+        }
+        public string WeekSpan { get { return FirstDay.ToString() + " - " + LastDay.ToString(); }  }
+        
         internal void ClearAllFood()
         {
             for(int i=0;i<7;i++)
             {
                 DaysOfTheWeek[i].ClearAllFood();
             }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
     }
 }
--- FoodCalculator/ViewModels/CalculatorViewModel.cs
+++ FoodCalculator/ViewModels/CalculatorViewModel.cs
@@ -29,15 +29,17 @@ namespace FoodCalculator.ViewModels
         //index 0-2 is for day 1
         //index 3-5 is for day 2 etc
         public ObservableCollection<string> MealFillings { get; set; } = new ObservableCollection<string>();
-        public Week WeekForDisplaying { get; set; } = new Week();
+        public Week WeekForDisplaying { get; set; } 
+        private Week CurrentWeek { get; set; } = new Week(); 
+        private Week NextWeek { get; set; } = new Week();
         public CalculatorViewModel(NavigationStore navigationStore,DataStore dataStore) 
         {
-            
+            WeekForDisplaying = new Week(curwek());
             NavigationStore = navigationStore;
             DataStore = dataStore;            
             NavigateToSettings = new NavigateCommand<SettingsViewModel>(NavigationStore, () =>new SettingsViewModel(NavigationStore, DataStore));
             NavigateToAddFood = new NavigateCommand<AddFoodViewModel>(NavigationStore,()=> new AddFoodViewModel(NavigationStore,DataStore));
-            Calculator = new Calculator(WeekForDisplaying, DataStore.GetFoodList(), DataStore.DayTemplates, dataStore.GetFoodTypes());
+            Calculator = new Calculator(NextWeek, DataStore.GetFoodList(), DataStore.DayTemplates, dataStore.GetFoodTypes());
             CalculateFoodCommand = new ButtonCommand(obj => 
             {
                 Calculator.Calculate();
@@ -57,6 +59,17 @@ namespace FoodCalculator.ViewModels
                     MealFillings.Add(i.ToString());
             });
             
+        }
+        (DateOnly, DateOnly) curwek()
+        {
+            int day = Convert.ToInt32(DateTime.Now.DayOfWeek);
+            DateOnly fd = new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            fd = fd.AddDays(-day + 1);
+            DateOnly sd = new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            sd = sd.AddDays(7 - day);
+
+
+            return (fd, sd);
         }
     }
 }
